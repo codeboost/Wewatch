3d91ac99e6cb27845a65524aa2039c9c2ee9bc52
Florin Braghis <florin.braghis@gmail.com>
2012-03-02 11:30:23 +0100

    updated go.sh

--- go.sh
+++ go.sh
@@ -1,1 +1,1 @@
-sudo forever start -c iced -o /var/log/wewatch.log -e /var/log/wewatch.err.log server.coffee &
+sudo forever start -c iced -o /var/log/wewatch.log -e /var/log/wewatch.err.log server &
